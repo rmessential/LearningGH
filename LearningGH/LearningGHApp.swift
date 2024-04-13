e0f81d80a8d3d3f8672c20aa03c13a1752dc96e4
rudyalayon <rudyalayon@gmail.com>
2024-04-13 11:46:43 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Rudy Alayon on 2024-04-13.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
