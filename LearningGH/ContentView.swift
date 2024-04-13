0d7cf025a915d56c5f05f849c573d680a7dc90e8
rudyalayon <rudyalayon@gmail.com>
2024-04-13 14:55:55 -0400

    Hello World Again.

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, World")
+            Text("Hello, World Again")
         }
         .padding()
     }
